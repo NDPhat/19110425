77fded9b23de6b55f21a1f1fa3d7d129c33ced87
NDPhat <daiphatcbl@gmail.com>
2022-10-04 21:19:51 +0700

    first commit

---
+++ models/models.posts.js
@@ -0,0 +1,25 @@
+var posts = [{
+    id: 0,
+    title: 'Dai Phat the nao ?',
+    content: `Dai Phat Dep Trai`,
+    comments: [{
+        user: 'Hai Duong',
+        comment: 'Dung !!'
+    }, {
+        user: 'Van Tinh',
+        comment: 'Co that khong?'
+    }]
+}, {
+    id: 1,
+    title: 'Hai Duong gio o dau?',
+    content: `Trong lanh tho Viet Nam!!`,
+    comments: [{
+        user: 'Dai Phat',
+        comment: 'Bach khoa HN?'
+    }, {
+        user: 'Van Tinh',
+        comment: `Vin ??`
+    }]
+}]
+
+module.exports = posts
